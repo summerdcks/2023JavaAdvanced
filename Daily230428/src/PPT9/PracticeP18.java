60bf959e721667b557f9b1576b10c7af2a1e41ee
summerdcks <summerdcks@gmail.com>
2023-04-30 17:45:09 +0900

    230428 JSON 실습

--- Daily230428/src/PPT9/PracticeP18.java
+++ Daily230428/src/PPT9/PracticeP18.java
@@ -8,33 +8,39 @@ import org.json.simple.JSONObject;
 
 public class PracticeP18 {
 
-	public static JSONObject oneRec(String name_k36, int studentid_k36, int kor_k36, int eng_k36, int mat_k36) {
-		JSONObject dataObject_k36 = new JSONObject();
-		dataObject_k36.put("name", name_k36);
-		dataObject_k36.put("studentid", studentid_k36);
+	public static JSONObject oneRec(String name_k36, int studentid_k36, int kor_k36, 
+															int eng_k36, int mat_k36) {
+		//JSON 형식으로 하나의 학생의 정보를 담은 JSONObject를 반환하는 메소드
+		JSONObject dataObject_k36 = new JSONObject();	//새로운 JSONObject 객체 dataObject 생성
+		dataObject_k36.put("name", name_k36);	//dataObject에 학생 이름 추가
+		dataObject_k36.put("studentid", studentid_k36);	//dataObject에 학생 학번 추가
 
-		JSONArray score_k36 = new JSONArray();
-		score_k36.add(kor_k36);
-		score_k36.add(eng_k36);
-		score_k36.add(mat_k36);
-		dataObject_k36.put("score", score_k36);
+		JSONArray score_k36 = new JSONArray();	//JSONArray 객체 score 생성
+		score_k36.add(kor_k36);	//score에 국어 점수 추가
+		score_k36.add(eng_k36);	//score에 영어 점수 추가
+		score_k36.add(mat_k36);	//score에 수학 점수 추가
+		dataObject_k36.put("score", score_k36);	//dataObject에 점수정보 추가
 
-		return dataObject_k36;
+		return dataObject_k36;	//dataObject 객체를 반환
 	}
 
 	public static void main(String[] args) {
-		JSONObject jsonObject_k36 = new JSONObject();
-		JSONArray datasArray_k36 = new JSONArray();
-		JSONObject dataObject_k36 = new JSONObject();
-		dataObject_k36.put("name", "나연");
-		dataObject_k36.put("studentid", 209901);
-		JSONArray score_k36 = new JSONArray();
-		score_k36.add(90);
-		score_k36.add(100);
-		score_k36.add(95);
-		dataObject_k36.put("score", score_k36);
-		datasArray_k36.add(dataObject_k36);
+		//JSON 형식으로 학생들의 정보를 담은 JSONArray를 생성하고, 파일로 저장하는 메소드
+		JSONObject jsonObject_k36 = new JSONObject();	//JSONObject 객체 jsonObject 생성
+		JSONArray datasArray_k36 = new JSONArray();	//JSONArray 객체 datasArray 생성
+		JSONObject dataObject_k36 = new JSONObject();	
+		//학생 정보 JSONObject 객체 dataObject 생성학생
+		dataObject_k36.put("name", "나연");	//학생 이름 추가	
+		dataObject_k36.put("studentid", 209901);	//학번 추가
+		JSONArray score_k36 = new JSONArray();	//JSONArray 객체 score 추가
+		score_k36.add(90);	//점수 추가
+		score_k36.add(100);	//점수 추가
+		score_k36.add(95);	//점수 추가
+		dataObject_k36.put("score", score_k36);	//dataObject에 점수정보 추가
+		datasArray_k36.add(dataObject_k36);	//학생 정보를 datasArray에 추가
 
+		//JSONObject 객체인 oneRec를 생성하고, 학생 이름, 학번, 점수 정보를 추가한 후,
+		//학생 정보 JSONArray 객체인 datasArray에 추가
 		datasArray_k36.add(oneRec("정연", 209902, 100, 85, 75));
 		datasArray_k36.add(oneRec("모모", 209903, 90, 75, 85));
 		datasArray_k36.add(oneRec("사나", 209904, 90, 85, 75));
@@ -44,17 +50,19 @@ public class PracticeP18 {
 		datasArray_k36.add(oneRec("채영", 209908, 100, 75, 95));
 		datasArray_k36.add(oneRec("쯔위", 209909, 80, 65, 95));
 
-		FileWriter file_k36;
+		FileWriter file_k36;	//JSON 파일을 쓰기 위한 FileWriter 객체 생성
 		try {
 			file_k36 = new FileWriter("C:\\Users\\DataSWB\\Desktop\\test.json");
-			file_k36.write(datasArray_k36.toJSONString());
-			file_k36.flush();
-			file_k36.close();
-		} catch (IOException e_k36) {
+			//FileWriter클래스를 이용하여 해당 경로에 파일을 생성
+			file_k36.write(datasArray_k36.toJSONString());	
+			//datasArray 객체를 문자열로 변환하여 파일에 작성
+			file_k36.flush();	//파일에 내용을 강제로 작성
+			file_k36.close();	//파일 닫기
+		} catch (IOException e_k36) {	//에러 처리
 			// TODO Auto-generated catch block
-			e_k36.printStackTrace();
+			e_k36.printStackTrace();	//에러정보 출력
 		}
-		System.out.println("JSON만든거 : " + datasArray_k36);
+		System.out.println("JSON만든거 : " + datasArray_k36);	//해당 문구와 datasArray 출력
 	}
 
 }
--- Daily230428/src/PPT9/PracticeP20.java
+++ Daily230428/src/PPT9/PracticeP20.java
@@ -9,31 +9,34 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class PracticeP20 {
+public class PracticeP20 {	//클래스 선언
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
-		JSONParser parser_k36 = new JSONParser();
+		//메인함수 선언
+		JSONParser parser_k36 = new JSONParser();	//JSONParser 객체 parser 생성
 
 		Object obj_k36 = parser_k36.parse(new FileReader("C:\\Users\\DataSWB\\Desktop\\test.json"));
+		//pc에서 해당 경로의 파일로부터 JSON 데이터를 읽어와 obj로 선언
 
-		JSONArray array_k36 = (JSONArray) obj_k36;
+		JSONArray array_k36 = (JSONArray) obj_k36;	//ob를 객체배열형 변환한 JSONArray 객체 array 선언
 
-		System.out.println("******************************");
-		for (int i_k36 = 0; i_k36 < array_k36.size(); i_k36++) {
-			JSONObject result_k36 = (JSONObject) array_k36.get(i_k36);
-			System.out.println("이름 : " + result_k36.get("name"));
-			System.out.println("학번 : " + result_k36.get("studentid"));
+		System.out.println("******************************");	//구분선 출력
+		for (int i_k36 = 0; i_k36 < array_k36.size(); i_k36++) { //array객체배열의 변수를 하나씩 살펴봄
+			JSONObject result_k36 = (JSONObject) array_k36.get(i_k36);	
+			//array객체배열에서 i번째 인덱스의 JSON 객체 추출하여 result로 선언
+			System.out.println("이름 : " + result_k36.get("name"));	//result의 name항목 출력
+			System.out.println("학번 : " + result_k36.get("studentid"));	//result의 studentid항목 출력
 
-			JSONArray score_k36 = (JSONArray) result_k36.get("score");
-			long kor_k36 = (long) score_k36.get(0);
-			long eng_k36 = (long) score_k36.get(1);
-			long mat_k36 = (long) score_k36.get(2);
-			System.out.println("국어 : " + kor_k36);
-			System.out.println("영어 : " + eng_k36);
-			System.out.println("수학 : " + mat_k36);
-			System.out.println("총점 : " + (kor_k36 + eng_k36 + mat_k36));
-			System.out.println("평균 : " + (kor_k36 + eng_k36 + mat_k36) / 3.0);
-			System.out.println("******************************");
+			JSONArray score_k36 = (JSONArray) result_k36.get("score"); 	//result에서 점수 배열 추출하여 score로 선언
+			long kor_k36 = (long) score_k36.get(0);	//score의 0번째 인덱스 항목인 국어점수를 정수형 변수로 선언
+			long eng_k36 = (long) score_k36.get(1);//score의 1번째 인덱스 항목인 영어점수를 정수형 변수로 선언
+			long mat_k36 = (long) score_k36.get(2);//score의 2번째 인덱스 항목인 수학점수를 정수형 변수로 선언
+			System.out.println("국어 : " + kor_k36);	//국어점수 출력
+			System.out.println("영어 : " + eng_k36);	//영어점수 출력
+			System.out.println("수학 : " + mat_k36);	//수학점수 출력
+			System.out.println("총점 : " + (kor_k36 + eng_k36 + mat_k36));	//총점 출력
+			System.out.println("평균 : " + (kor_k36 + eng_k36 + mat_k36) / 3.0);	//평균 출력
+			System.out.println("******************************");	//구분선 출력
 		}
 	}
 }
